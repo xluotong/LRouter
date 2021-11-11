b570ebd80dc718ce102489daab5e272198e5c07d
luotong <luotong@bilibili.com>
2021-11-11 18:46:48 +0800

    update

--- lrouter-api/src/main/java/com/billbook/lib/router/Injector.kt
+++ lrouter-api/src/main/java/com/billbook/lib/router/Injector.kt
@@ -1,18 +1,9 @@
 package com.billbook.lib.router
 
-import android.app.Activity
-
 /**
  * @author xluotong@gmail.com
  */
 interface Injector<T> {
 
     fun inject(target: T)
-}
-
-class MainActivityInject : Injector<Activity> {
-    override fun inject(target: Activity) {
-        // 怎么区分是服务还是extra,extra用Autowired
-        // 收集类Field
-    }
 }
